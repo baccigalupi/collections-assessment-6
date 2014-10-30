fa24a0229c8ccadde095ee55088c9e3df0f1d622
Cameron Irmas <cjirmas@gmail.com>
2014-10-30 15:15:25 -0700

    complete collection

---
+++ collections.rb
@@ -0,0 +1,66 @@
+student_list =
+[{"john" => {
+    first_name: "John",
+    last_name: "Foley",
+    email: "john@gschool.it",
+    class: "Beginning snark"
+  }},
+  {"sylwester" => {
+    first_name: "Sylwester",
+    last_name: "Kelsey",
+    email: "sellie@gmail.com",
+    class: "Ruby Immersive"
+    }
+  },
+  {"timothy" => {
+    first_name: "Timothy",
+    last_name: "Rama",
+    email: "tim.rama@gmail.com",
+    class: "Ruby Immersive"
+    }
+  },
+  {"kane" => {
+    first_name: "Kane",
+    last_name: "Baccigalupi",
+    email: "kane@gschool.it",
+    class: "C for dummies"
+    }
+  },
+  {"nikita" => {
+    first_name: "Nikita",
+    last_name: "Theodosius",
+    email: "nikita.theo@gmail.com",
+    class: "Ruby Immersive"
+    }
+  },
+  {"roddy" => {
+    first_name: "Roddy",
+    last_name: "Eldred",
+    email: "roddy.el@gmail.com",
+    class: "Ruby Immersive"
+    }
+  },
+  {"martha" => {
+    first_name: "Martha",
+    last_name: "Berner",
+    email: "martha@gschool.it",
+    class: "Time travel for beginners"
+    }
+  },
+  {"martha" => {
+    first_name: "Martha",
+    last_name: "Berner",
+    email: "martha@gschool.it",
+    class: "Time travel for beginners"
+    }
+  },
+  {"kofi" => {
+    first_name: "Kofi",
+    last_name: "Thomas",
+    email: "k.thomas@hotmail.com",
+    class: "Ruby Immersive"
+    }
+  }
+]
+
+p student_list
